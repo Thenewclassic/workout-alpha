b9a8ed933e3062551873d54d690fe17c2ab9c003
Carlos Escobar <carlos.b.escobar@gmail.com>
2017-02-26 15:52:32 -0500

    components for each of the initial pages for the POC

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,14 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import ActivityStreamPage from './components/activity-stream/ActivityStreamPage';
 import WorkoutListPage from './components/workout/WorkoutListPage';
+import SchedulePage from './components/schedule/SchedulePage';
+import ProfilePage from './components/profile/ProfilePage';
 
 export default (
     <Route path="/" component={App}>
         <IndexRoute component={ActivityStreamPage} />
         <Route path="/workouts" component={WorkoutListPage} />
+        <Route path="/schedule" component={SchedulePage} />
+        <Route path="/profile" component={ProfilePage} />
     </Route>
 );
